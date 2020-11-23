2a97e1d2780650804851233df24b850600b42789
Derek Chen <derekchen72@gmail.com>
2020-11-23 17:34:41 -0500

    worked on hash project

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    hello = 0
+    self.each_with_index.inject(0) do |acc, (ele, i)|
+      hello = (ele.hash + i.hash) ^ acc
+    end
+    p hello
   end
 end
 
+
 class String
   def hash
+    alpha = ("a".."z").to_a
+    upper_alpha = ("A".."Z").to_a
+    final = alpha + upper_alpha
+    self.split("").map(&:final.index).hash
+
   end
 end
 
